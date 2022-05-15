5c79d35db58f07f04c556aceacab9ea5f4f13c84
Laretesaderat <98397896+Laretesaderat@users.noreply.github.com>
2022-05-15 13:44:48 +0300

    change search

--- mysite/books/views.py
+++ mysite/books/views.py
@@ -15,22 +15,27 @@ class SearchView(View):
     def get(self,request):
         context={}
         q=request.GET.get('q')
+        lines_raw=str({q})[2:-2]
+        lines=lines_raw.split()
         if q:
             query=[]
             books=Book.objects.order_by('date')
             for book in books:
-                for genre in book.genres.all():
-                    if re.match(str({q})[2:-2],str(genre)):
-                        query.append(book)
-                        break
-                for author in book.authors.all():
-                    if re.match(str({q})[2:-2],str(author)):
-                        query.append(book)
-                        break
-
+                for line in lines:
+                    for genre in book.genres.all():
+                        if re.search(line, str(genre)):
+                            query.append(book)
+                            break
+                    for author in book.authors.all():
+                        if re.search(line, str(author)):
+                            query.append(book)
+                            break
+            query_new=[]
+            for i in query:
+                if i not in query_new:
+                    query_new.append(i)
             context['last_question'] = '?q=%s' % q
-            print(query)
-            current_page = Paginator(query, 1)
+            current_page = Paginator(query_new, 10)
 
             page = request.GET.get('page')
             try:
@@ -39,6 +44,5 @@ class SearchView(View):
                 context['object_list'] = current_page.page(1)
             except EmptyPage:
                 context['object_list'] = current_page.page(current_page.num_pages)
-
         return render(request=request, template_name=self.template_name, context=context)
 
